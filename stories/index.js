afb8d1042e1ecdb00c3f8b1da2845a14935a31e5
sakane <reshi.sus316@gmail.com>
2026-06-24 09:29:08 +0900

    黄昏の校舎ストーリー追加

--- stories/index.js
+++ stories/index.js
@@ -1,13 +1,19 @@
 import prologue from './prologue.js';
 import yuya_1 from './yuya_1.js';
+import yuya_2 from './yuya_2.js';
 import rabi_1 from './rabi_1.js';
 import rabi_2 from './rabi_2.js';
+import shizuku_1 from './shizuku_1.js';
+import yukika_1 from './yukika_1.js';
 
 const STORIES = {
   prologue,
   yuya_1,
+  yuya_2,
   rabi_1,
   rabi_2,
+  shizuku_1,
+  yukika_1,
 };
 
 export { STORIES };
