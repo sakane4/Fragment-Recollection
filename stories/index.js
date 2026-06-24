7311a2324066d8f5ea2dcea3973512b2ed509b09
sakane <reshi.sus316@gmail.com>
2026-06-24 18:59:45 +0900

    カオルストーリー追加

--- stories/index.js
+++ stories/index.js
@@ -5,6 +5,7 @@ import rabi_1 from './rabi_1.js';
 import rabi_2 from './rabi_2.js';
 import shizuku_1 from './shizuku_1.js';
 import yukika_1 from './yukika_1.js';
+import kaoru_1 from './kaoru_1.js';
 
 const STORIES = {
   prologue,
@@ -14,6 +15,7 @@ const STORIES = {
   rabi_2,
   shizuku_1,
   yukika_1,
+  kaoru_1,
 };
 
 export { STORIES };
